bc696eb1f8823b0bed01da2a75e0be87c1291df2
Dominik Honnef <dominikh@fork-bomb.org>
2012-01-31 18:27:54 +0100

    add support for blocks

--- lib/yard-sd/sequence_diagram/diagram.rb
+++ lib/yard-sd/sequence_diagram/diagram.rb
@@ -4,7 +4,8 @@ require "fileutils"
 module SequenceDiagram
 
   class Diagram
-    Call        = Struct.new(:p1, :p2, :message, :return)
+    Block = Struct.new(:level, :name, :description)
+    Call  = Struct.new(:p1, :p2, :message, :return, :block)
     @@uid = 0
 
     attr_reader :uid
@@ -15,7 +16,9 @@ module SequenceDiagram
     end
 
     def parse(s)
-      last_calls = Hash.new {|h, k| h[k] = []}
+      last_calls  = Hash.new {|h, k| h[k] = []}
+      blocks      = [Block.new(0, nil, nil)]
+      last_indent = 0
 
       s.each_line do |line|
         line.chomp!
@@ -28,25 +31,37 @@ module SequenceDiagram
           aliasing = $5 || name
           thread = $1 == "thread"
           @participants << Participant.new(name, aliasing, distance, thread)
-        when /^(.+?)\s*(->|-->)\s*(.+?):(?: (.+))?$/
-          p1      = @participants.find {|p| p.aliasing == $1}
-          p2      = @participants.find {|p| p.aliasing == $3}
+        when /^(\s*)block "([^"]+)" "([^"]+)"/
+          if $1.size < last_indent
+            blocks.pop
+          end
+
+          blocks << Block.new(blocks.size, $2, $3)
+        when /^(\s*)(.+?)\s*(->|-->)\s*(.+?):(?: (.+))?$/
+          p1      = @participants.find {|p| p.aliasing == $2}
+          p2      = @participants.find {|p| p.aliasing == $4}
 
           error(:unknown_participant, line) if p1.nil?
           error(:unknown_participant, line) if p2.nil?
 
-          message = $4
+          message = $5.to_s
 
-          case $2
+          if $1.size < last_indent
+            blocks.pop
+          end
+
+          last_indent = $1.size
+
+          case $3
           when "->"
-            call = Call.new(p1, p2, message, nil)
+            call = Call.new(p1, p2, message, nil, blocks.last)
             last_calls[p1] << call
             @calls << call
           when "-->"
             last_calls[p2].pop.return = message.to_s
             @calls << :pop
           end
-        when /^noreturn$/
+        when /^\s*noreturn$/
           @calls << :pop
         else
           error(:cannot_parse, line)
@@ -67,35 +82,50 @@ module SequenceDiagram
       end
 
       last_callee = nil
+      prev_block_level  = 0
       open = []
+
+      indent = 0
       @calls.each do |call|
         if call == :pop
-          # not nesting, close last call
-          indent = " " * (open.size - 1) * 2
-          s << indent << "\\end{#{open.pop}}\n"
+          indent -= 1
+          s << " " * indent << "\\end{#{open.pop}}\n"
           next
         end
 
-        indent = " " * open.size * 2
+        if call.block.level > prev_block_level
+          s << " " * indent << "\\begin{sdblock}{#{call.block.name}}{#{call.block.description}}\n"
+          indent += 1
+        elsif call.block.level < prev_block_level
+          indent -= 1
+          s << " " * indent << "\\end{sdblock}\n"
+        end
+        prev_block_level = call.block.level
 
         if !call.return.nil?
           if call.p1 == call.p2
-            s << indent << "\\begin{callself}{#{call.p1.uid}}{#{SequenceDiagram.latex_escape(call.message)}}{#{SequenceDiagram.latex_escape(call.return)}}\n"
+            s << " " * indent << "\\begin{callself}{#{call.p1.uid}}{#{SequenceDiagram.latex_escape(call.message)}}{#{SequenceDiagram.latex_escape(call.return)}}\n"
             open << :callself
           else
-            s << indent << "\\begin{call}{#{call.p1.uid}}{#{SequenceDiagram.latex_escape(call.message)}}{#{call.p2.uid}}{#{SequenceDiagram.latex_escape(call.return)}}\n"
+            s << " " * indent << "\\begin{call}{#{call.p1.uid}}{#{SequenceDiagram.latex_escape(call.message)}}{#{call.p2.uid}}{#{SequenceDiagram.latex_escape(call.return)}}\n"
             open << :call
           end
         else
-          s << indent << "\\begin{messcall}{#{call.p1.uid}}{#{SequenceDiagram.latex_escape(call.message)}}{#{call.p2.uid}}\n"
+          s << " "* indent << "\\begin{messcall}{#{call.p1.uid}}{#{SequenceDiagram.latex_escape(call.message)}}{#{call.p2.uid}}\n"
           open << :messcall
         end
+        indent += 1
       end
 
       while close = open.pop
-        close = "\\end{#{close}}\n"
-        indent = " " * open.size * 2
-        s << indent << close
+        s << " " * indent << "\\end{#{close}}\n"
+        indent -= 1
+      end
+
+      while prev_block_level > 0
+        indent -= 1
+        s << " " * indent << "\\end{sdblock}\n"
+        prev_block_level -= 1
       end
 
       s << "\\end{sequencediagram}"
